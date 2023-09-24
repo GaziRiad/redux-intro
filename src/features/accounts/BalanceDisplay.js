65dff54d5f539e0a42bf3fbd1468d2b5d80f0693
GaziRiad <ouhibimustapha36@gmail.com>
2023-09-24 11:55:30 +0100

    Making an API call with redux thunk

--- src/features/accounts/BalanceDisplay.js
+++ src/features/accounts/BalanceDisplay.js
@@ -1,3 +1,7 @@
+// import { useSelector } from "react-redux";
+
+import { connect } from "react-redux";
+
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
@@ -5,8 +9,17 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+  // const { balance } = useSelector((store) => store.account);
+
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+// The old way of connecting redux to react (before hooks !!)
+
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+export default connect(mapStateToProps)(BalanceDisplay);
--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,16 +2,24 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
 
     case "account/withdraw":
       // if (state.balance < action.payload) return { ...state };
-      return { ...state, balance: state.balance - action.payload };
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
 
     case "account/requestLoan":
       if (state.loan > 0) return state;
@@ -29,6 +37,10 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
+
     default:
       return { ...state };
   }
@@ -36,8 +48,21 @@ export default function accountReducer(state = initialStateAccount, action) {
 
 // ACTION CREATORS
 
-function deposit(amount = 0) {
-  return { type: "account/deposit", payload: amount };
+function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+
+    // API request
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    // return ACTION
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 function withdraw(amount = 0) {
   return { type: "account/withdraw", payload: amount };
